05295f10d9a5f8e787e69f7b4451071e2a446c2d
Meteor <97183228+ZKMeteor@users.noreply.github.com>
2023-02-02 05:05:01 +0800

    change anime

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ Vue.config.productionTip = false;
 let year = new Date().getFullYear();
 
 const footer = new Vue({
-el:'.footer',
-data:{
-    year:year,
-}
+    el:'.footer',
+    data:{
+        year,
+    }
 })
